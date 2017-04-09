859cf5e3f120dc84d13a9b28e1ed10c35da5e502
Sarudin <sarudin1wow@gmail.com>
2017-04-09 14:03:35 -0600

    Fixed another mixed-content issue

--- js/services/aboutExoService.js
+++ js/services/aboutExoService.js
@@ -1,6 +1,6 @@
 angular.module('whatsOutThereApp').service('aboutExoService', function($http) {
   this.getExoInfo = function(nameAndLetter) {
-    return $http.get("http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&where=pl_hostname like '" + nameAndLetter.substring(0, nameAndLetter.length - 1) + "' and pl_letter like '" + nameAndLetter.substring(nameAndLetter.length - 1, nameAndLetter.length) + "'&format=json").then(function(response) {
+    return $http.get("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&where=pl_hostname like '" + nameAndLetter.substring(0, nameAndLetter.length - 1) + "' and pl_letter like '" + nameAndLetter.substring(nameAndLetter.length - 1, nameAndLetter.length) + "'&format=json").then(function(response) {
       return response.data;
     })
   }
