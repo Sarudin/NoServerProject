1b261596b9decca8868d6b64d1084f81c8b940e6
Sarudin <sarudin1wow@gmail.com>
2017-04-09 14:05:20 -0600

    Fixed yet another mixed-content issue

--- js/directives/aboutExoFunctionalDir.js
+++ js/directives/aboutExoFunctionalDir.js
@@ -14,7 +14,7 @@ angular.module('whatsOutThereApp').directive('aboutExoFunctionalDir', function()
       $scope.getExoInfo();
 
       $scope.showLocation = function(idx) {
-        window.open('http://server3.sky-map.org/v2?zoom=1&show_grid=1&show_constellation_lines=1&show_constellation_boundaries=1&show_const_names=0&show_galaxies=1&show_box=1&box_ra=19.883874999999996&box_de=47.60494444444444&box_width=50&box_height=50&img_source=DSS2&ra=' + idx.ra_str + '&de=' + idx.dec_str);
+        window.open('https://server3.sky-map.org/v2?zoom=1&show_grid=1&show_constellation_lines=1&show_constellation_boundaries=1&show_const_names=0&show_galaxies=1&show_box=1&box_ra=19.883874999999996&box_de=47.60494444444444&box_width=50&box_height=50&img_source=DSS2&ra=' + idx.ra_str + '&de=' + idx.dec_str);
       }
     }
   }
