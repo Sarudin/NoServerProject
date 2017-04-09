667d2019fa4f7bec22d34f3658bc7998f06c8ef5
Sarudin <sarudin1wow@gmail.com>
2017-04-09 14:01:38 -0600

    Fixed mixed-content issue.

--- js/services/allExoService.js
+++ js/services/allExoService.js
@@ -1,6 +1,6 @@
 angular.module('whatsOutThereApp').service('allExoService', function($http) {
   this.getAllExo = function() {
-    return $http.get('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&format=json').then(function(response) {
+    return $http.get('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&format=json').then(function(response) {
       return response.data.slice(70, 100);
     })
   }
